2fd4a184ff5fe4a102b00be59d40d39b021db656
yvanzo <yvanzo@metabrainz.org>
2020-03-20 10:15:02 +0100

    Fix setting react-table accessor for count column
    * `accessor` is mandatory in options for column with value,
      e.g., it can be used to sort non-paginated tables later on.
    * `Cell` defaults to returning `String(accessor(row))`.
    
    See https://github.com/tannerlinsley/react-table/blob/v7.0.0/docs/api/useTable.md#column-options

--- root/utility/tableColumns.js
+++ root/utility/tableColumns.js
@@ -165,9 +165,9 @@ export function defineCountColumn<D>(
   className?: string,
 ): ColumnOptions<D, number> {
   return {
-    Cell: ({row: {original}}) => (
+    Cell: ({cell: {value}}) => (
       <CatalystContext.Consumer>
-        {($c: CatalystContextT) => formatCount($c, getCount(original))}
+        {($c: CatalystContextT) => formatCount($c, value)}
       </CatalystContext.Consumer>
     ),
     Header: (sortable
@@ -179,6 +179,7 @@ export function defineCountColumn<D>(
         />
       )
       : title),
+    accessor: row => getCount(row),
     className: className || 'count c',
     id: columnName,
   };
